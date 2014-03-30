1edfae147701a08d61b4d21fd7f49a00045b3e95
William Mantegna <wmantegna@gmail.com>
2014-03-30 12:16:32 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RorBlog::Application.config.secret_key_base = 'a14778defe7d6cdb04d137786542b7390d0582d50ed36d1a2fdf3792a6ec153c9b8b42764daa464675e7e231047fdf1e548a4406e25411c93f1df234d293e3e9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RorBlog::Application.config.session_store :cookie_store, key: '_ror-blog_session'
